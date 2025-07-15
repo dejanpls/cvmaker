8fe5776d1174c130007c92435e54f444893fbc55
dejanpls <68787353+dejanpls@users.noreply.github.com>
2025-07-15 10:53:04 +0200

    refactor: declutter PersonalForm with Input component

--- src/components/PersonalForm.jsx
+++ src/components/PersonalForm.jsx
@@ -1,3 +1,5 @@
+import Input from "./Input";
+
 export default function PersonalForm() {
   return (
     <section aria-labelledby="personalDetailsHeading">
@@ -6,48 +8,23 @@ export default function PersonalForm() {
       <fieldset className="fullName">
         <legend className="sr-only">Full Name</legend>
 
-        <div className="input">
-          <label htmlFor="firstName">First Name</label>
-          <input type="text" id="firstName" name="firstName" />
-        </div>
-
-        <div className="input">
-          <label htmlFor="lastName">Last Name</label>
-          <input type="text" id="lastName" name="lastName" />
-        </div>
+        <Input name={"given-name"} label={"First Name"} />
+        <Input name={"family-name"} label={"Last Name"} />
       </fieldset>
 
       <fieldset className="contact">
         <legend className="sr-only">Contact</legend>
 
-        <div className="input">
-          <label htmlFor="email">Email</label>
-          <input type="email" id="email" name="email" />
-        </div>
-
-        <div className="input">
-          <label htmlFor="phone">Phone</label>
-          <input type="tel" id="phone" name="phone" />
-        </div>
+        <Input name={"email"} label={"Email"} />
+        <Input name={"tel"} label={"Phone"} />
       </fieldset>
 
       <fieldset className="location">
         <legend className="sr-only">Location</legend>
 
-        <div className="input">
-          <label htmlFor="address">Address</label>
-          <input type="text" id="address" name="address" />
-        </div>
-
-        <div className="input">
-          <label htmlFor="city">City</label>
-          <input type="text" id="city" name="city" />
-        </div>
-
-        <div className="input">
-          <label htmlFor="country">Country</label>
-          <input type="text" id="country" name="country" />
-        </div>
+        <Input name={"street-address"} label={"Address"} />
+        <Input name={"address-level2"} label={"City"} />
+        <Input name={"country"} label={"Country"} />
       </fieldset>
     </section>
   );
